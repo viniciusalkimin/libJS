988aac5dff8762a9402713f7e0c95a5739b3d230
vinicius.alkimin <vinicius.alkimin@deal.com.br>
2023-03-20 19:44:56 -0300

    Add tratamento de exceptions

--- index.js
+++ index.js
@@ -3,11 +3,19 @@ import fs from 'fs';
 
 console.log(chalk.blue('olá mundo'));
 
+function tratarErro(erro) {
+    console.log(erro);
+    throw new Error(chalk.red(erro));
+}
+
 const encoding = 'utf-8';
 function arquivoHandler(caminhoArquivo) {
-    fs.readFile(caminhoArquivo, encoding, (_, texto ) => {
+    fs.readFile(caminhoArquivo, encoding, (erro, texto ) => {
+        if(erro) {
+            tratarErro(erro);
+        }
         console.log(chalk.bgGrey(texto));
     });
 }
 
-arquivoHandler('./arquivos/texto.md');
+arquivoHandler('./arquivos/');
